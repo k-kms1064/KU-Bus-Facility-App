c3e0d2829317b39a1d695237b301ec5e58ddeb4e
k-kms1064 <kms106418@gmail.com>
2025-11-23 17:43:21 +0900

    feat: Favorite 화면 Activity 기본 뼈대 생성

---
+++ app/src/main/java/com/konkuk/ottae/favorite/FavoriteListActivity.kt
@@ -0,0 +1,12 @@
+package com.konkuk.ottae.favorite
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FavoriteListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(android.R.layout.simple_list_item_1)
+    }
+}
