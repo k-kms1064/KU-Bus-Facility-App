ebefe2e9f4e2ce66dc54fcc8b7367e3521b87f78
k-kms1064 <kms106418@gmail.com>
2025-11-23 17:50:19 +0900

    feat: BusArrivalActivity RecyclerView 초기 연결

--- app/src/main/java/com/konkuk/ottae/bus/BusArrivalActivity.kt
+++ app/src/main/java/com/konkuk/ottae/bus/BusArrivalActivity.kt
@@ -2,11 +2,21 @@ package com.konkuk.ottae.bus
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.konkuk.ottae.R
 
 class BusArrivalActivity : AppCompatActivity() {
+
+    private lateinit var recycler: RecyclerView
+    private val adapter = BusAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bus_arrival)
 
-        setContentView(android.R.layout.simple_list_item_1)
+        recycler = findViewById(R.id.recyclerBusArrival)
+        recycler.layoutManager = LinearLayoutManager(this)
+        recycler.adapter = adapter
     }
 }
