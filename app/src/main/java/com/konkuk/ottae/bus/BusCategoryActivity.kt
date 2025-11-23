af9294486f32eaa130c6abf5016c027791fab1c7
k-kms1064 <kms106418@gmail.com>
2025-11-23 17:41:54 +0900

    feat: Bus 기능 Activity 기본 뼈대 생성

---
+++ app/src/main/java/com/konkuk/ottae/bus/BusCategoryActivity.kt
@@ -0,0 +1,12 @@
+package com.konkuk.ottae.bus
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BusCategoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(android.R.layout.simple_list_item_1)
+    }
+}
