3aa6acbf46d39fb0deb11aa1505aa71b5e1fde19
k-kms1064 <kms106418@gmail.com>
2025-11-23 18:50:03 +0900

    refactor: BusItem 코드 구조 정리

--- app/src/main/java/com/konkuk/ottae/bus/BusItem.kt
+++ app/src/main/java/com/konkuk/ottae/bus/BusItem.kt
@@ -1,6 +1,9 @@
 package com.konkuk.ottae.bus
 
 data class BusItem(
+    val routeId: String,
     val routeName: String,
-    val arrivalInfo: String
+    val minutes: Int,
+    val stopsAway: Int,
+    val direction: String
 )
