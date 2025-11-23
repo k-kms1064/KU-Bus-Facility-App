45b032280135286b48349e968a4a5cf49bdfd797
k-kms1064 <kms106418@gmail.com>
2025-11-23 17:42:43 +0900

    feat: Facility 화면 Activity 기본 뼈대 생성

---
+++ app/src/main/java/com/konkuk/ottae/facility/FacilityCategoryActivity.kt
@@ -0,0 +1,12 @@
+package com.konkuk.ottae.facility
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FacilityCategoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(android.R.layout.simple_list_item_1)
+    }
+}
