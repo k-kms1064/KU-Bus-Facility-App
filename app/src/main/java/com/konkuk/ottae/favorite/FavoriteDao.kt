e608f7e15039ff01d2cadc5aae249735fcbecb53
k-kms1064 <kms106418@gmail.com>
2025-11-23 18:52:58 +0900

    refactor: FavoriteDao 코드 구조 정리

--- app/src/main/java/com/konkuk/ottae/favorite/FavoriteDao.kt
+++ app/src/main/java/com/konkuk/ottae/favorite/FavoriteDao.kt
@@ -2,14 +2,21 @@ package com.konkuk.ottae.favorite
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Delete
 import androidx.room.Query
 
 @Dao
 interface FavoriteDao {
 
-    @Query("SELECT * FROM FavoriteEntity")
-    fun getAll(): List<FavoriteEntity>
-
     @Insert
-    fun insert(item: FavoriteEntity)
+    suspend fun insertFavorite(favorite: FavoriteEntity)
+
+    @Delete
+    suspend fun deleteFavorite(favorite: FavoriteEntity)
+
+    @Query("SELECT * FROM favorites")
+    suspend fun getAllFavorites(): List<FavoriteEntity>
+
+    @Query("SELECT * FROM favorites WHERE facilityId = :facilityId LIMIT 1")
+    suspend fun getFavoriteByFacilityId(facilityId: Int): FavoriteEntity?
 }
